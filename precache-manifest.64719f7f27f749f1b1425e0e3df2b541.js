d025b95c3cccbceb3a9efb375a9634190e4627fe
MuhammadHaris2 <mhari8339@gmail.com>
2020-10-08 22:32:11 +0500

    Updates

---
+++ precache-manifest.64719f7f27f749f1b1425e0e3df2b541.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4291f69cb23ab04e926f591a1bcb17c5",
+    "url": "/Zological-website/index.html"
+  },
+  {
+    "revision": "ec0a043972a422cbd4ad",
+    "url": "/Zological-website/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "9374a08585166ac187d3",
+    "url": "/Zological-website/static/css/main.dad4c55b.chunk.css"
+  },
+  {
+    "revision": "ec0a043972a422cbd4ad",
+    "url": "/Zological-website/static/js/2.17dce9f3.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Zological-website/static/js/2.17dce9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9374a08585166ac187d3",
+    "url": "/Zological-website/static/js/main.8133b122.chunk.js"
+  },
+  {
+    "revision": "a19c1f3c1c29bc396be6",
+    "url": "/Zological-website/static/js/runtime-main.395363ec.js"
+  },
+  {
+    "revision": "ac271de883faa03617b212beeda73db3",
+    "url": "/Zological-website/static/media/1.ac271de8.jpg"
+  },
+  {
+    "revision": "c6686e389c6e940600c9080747bc34da",
+    "url": "/Zological-website/static/media/2.c6686e38.jpg"
+  },
+  {
+    "revision": "79604c9bfd9c6578ebeed3ec813c5028",
+    "url": "/Zological-website/static/media/s1.79604c9b.jpg"
+  },
+  {
+    "revision": "9c292b63481ea546f51f4ec0bf004c87",
+    "url": "/Zological-website/static/media/s2.9c292b63.jpg"
+  },
+  {
+    "revision": "1f85a39dd5b30f6d29e979236d864000",
+    "url": "/Zological-website/static/media/s3.1f85a39d.jpg"
+  },
+  {
+    "revision": "d946c46ed9b9182582d46c68b6295aee",
+    "url": "/Zological-website/static/media/s4.d946c46e.jpg"
+  },
+  {
+    "revision": "dd49d943c2f8ddf6dfaa3558e9053389",
+    "url": "/Zological-website/static/media/s5.dd49d943.jpg"
+  },
+  {
+    "revision": "5001dc9829ae0f0305fcb96179973b71",
+    "url": "/Zological-website/static/media/s7.5001dc98.jpg"
+  }
+]);
